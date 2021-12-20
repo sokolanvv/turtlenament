563cf701ec70aac5659c6b63d202c835792df737
sokolan.vv <sokolan.vv@gmail.com>
2021-12-21 03:23:41 +1000

    5

--- module1.sh
+++ module1.sh
@@ -19,6 +19,7 @@ p () {
 echo -n > file1
 echo -n > file2
 echo -n > file3
+echo -n > file4
 
 p "" 4
 p "Версия дистрибутива ROS" file1
@@ -41,5 +42,38 @@ p "Список сервисов на роботе" file2
 rosservice list >> file2
 
 p "Подключенные камеры" file3
-ls -la /dev/video* >> file3
+v4l2-ctl --list-devices
+p "Разрешение камеры" file3
+v4l2-ctl --device=/dev/video0 --all | grep Width/Height
 
+p "Сброс одометрии" file4
+p "Move robot a bit and call rostopic echo /odom_pose2d" file4
+p "Try following: rosservice call /reset to reset ODOM" file4
+p "Move robot a bit more and call rostopic echo /odom_pose2d" file4
+
+p "" file4 4
+p "Voltage check: rostopic echo /bat" file4
+p "" file4 4
+p "IMU: rostopic echo /imu" file4
+p "HZ IMU: rostopic hz /imu" file4
+p "Данные о ковариации не заполняются. Данные публикуются с частотой 20 Герц" file4 4
+
+p "Import that everything connected to rviz is probably connected to host, NOT THE BOT" file4
+p "So disconnect from the bot and install the plugin on host machine" file4
+p "For imu plugin for rviz try: sudo apt-get install ros-noetic-imu-tools" file4
+p "If that don' work try the following: " file4
+p "git clone https://github.com/ccny-ros-pkg/imu_tools.git /home/pi/imu_tools" file4 0
+p "export ROS_PACKAGE_PATH=/home/pi/ros/ros-pkg:/home/pi/imu_tools/imu_tools" file4 0
+p "rosdep install imu_tools" file4 0
+p "rosmake imu_tools" file4 0
+
+p "Don't forget the all new IP, don't forget to source the bash too!" file4
+p "When in rviz change fixed frame to imu_link" file4
+
+p "Goto: https://github.com/voltbro/ws-sro/tree/main/HEX" file4
+wget https://raw.githubusercontent.com/voltbro/ws-sro/main/HEX/TB-RGBline.ino.mega.hex
+p "Uh-oh it's already downloaded tee-hee \( > O<)/" file4
+p "Don't forget to PLUG IN the cable for MEGA!" file4
+p "Run the: avrdude -v -v -p atmega2560 -c wiring -P /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0 -b 115200 -D -U flash:w:TB-RGBline.ino.mega.hex:i" file4 1
+
+echo "Good Job! You won a ballon and some respecc maybe ,(o 3 o)-"
---
+++ prereqs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ip a | grep "inet 192" > ip_number
+mkdir ~/.ssh
+touch ~/.ssh/authorized_keys
+
+wget https://raw.githubusercontent.com/sokolanvv/turtlenament/master/module1.sh
